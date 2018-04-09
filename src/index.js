64ab852fb7205a0155350f6bf01196888fd7a13f
Ignacio Carvajal Duran <ignacio.carvajal@inacapmail.cl>
2018-04-09 17:33:01 -0300

    app task finish

---
+++ src/index.js
@@ -0,0 +1,34 @@
+'use strict'
+const cors = require('cors');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//import rutes
+// const route = require('./routes/index');
+const taskRoute = require('./routes/task');
+
+//setting
+// app.set('views', path.join(__dirname,'./views'));
+app.set('port', process.env.PORT || 3000);
+// app.engine('html', require('ejs').renderFile);
+// app.set('View engine', 'ejs');
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//route
+// app.use(route);
+app.use('/api', [taskRoute]);
+
+
+//static files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+//start server
+app.listen(3000, ()=>{
+    console.log('server on port 3000');
+});
