61f3ea37ccd38433845e22301c2b4a42551fd262
Ignacio Carvajal Duran <ignacio.carvajal@inacapmail.cl>
2018-04-10 12:32:46 -0300

    test 5

--- src/index.js
+++ src/index.js
@@ -29,9 +29,6 @@ app.use('/api', [taskRoute]);
 //static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
 
 //start server
 app.listen(port, ()=>{
