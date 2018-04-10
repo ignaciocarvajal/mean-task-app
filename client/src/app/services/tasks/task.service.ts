b482dc43eb85f260f9f589f60fe1063bf2a8f760
Ignacio Carvajal Duran <ignacio.carvajal@inacapmail.cl>
2018-04-10 15:40:38 -0300

    update domain

--- client/src/app/services/tasks/task.service.ts
+++ client/src/app/services/tasks/task.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class TaskService {
 
-  private domain = 'http://localhost:3000/';
+  private domain = 'http://app-task-mean.herokuapp.com/';
 
   constructor(private http: HttpClient) { }
 
